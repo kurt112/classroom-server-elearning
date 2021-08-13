2dd711d34a63e2ce903d123eaa782041c7e3c621
kurt112 <kurtorioque112@gmail.com>
2021-08-13 09:29:24 +0800

    Add Video Call

--- socketIO/Socket.js
+++ socketIO/Socket.js
@@ -7,7 +7,9 @@ const {
 } = require('../utils/users');
 
 const moment = require('moment')
+const {use} = require("express/lib/router");
 const m = moment()
+const videos = new Map()
 const socket = (io) => {
     io.on('connection', (socket) => {
 
@@ -16,26 +18,27 @@ const socket = (io) => {
             userJoinClass(socket.id, name, role, path,classes)
             socket.join(path)
 
-            const usersFilter = getClassData(path).users.filter(e => e.id !== socket.id)
-            const data = {
-                ...getClassData(path),
-                usersFilter
-            }
 
+            // for getting the data in dashboard
             io.emit('dashboardData', getClasses())
 
-            io.to(path).emit('classData', data);
+            /// for getting the chats
+            io.to(path).emit('classData', getClassData(path));
 
 
+            // for getting the send message
             socket.on('sendMessage', (message, time, isAnnouncement, setMessage) => {
                 if (setMessage !== undefined) setMessage()
                 io.to(path).emit('messages', sendMessage(path, message, name, time, isAnnouncement))
             })
 
+            // to get all users online and offlne
             socket.on('users',() => {
                 io.to(path).emit('getUsers', {...getClassData(path)})
             })
 
+
+            // handle disconnection
             socket.on('disconnect', () => {
                 io.to(path).emit('user-disconnected', socket.id)
                 io.to(path).emit('messages', sendMessage(path, name + ' Has Left The Class ', name, m.format('h:mm a'), true))
@@ -49,27 +52,40 @@ const socket = (io) => {
         })
 
 
-
-
+        // get all data when dashbaord load
         socket.on('dashboard', () =>{
             io.emit('dashboardData',getClasses())
         })
 
 
+        // joining in video
+        socket.on('join-video', (path,userId) => {
+            const id = socket.id
+            socket.join(path)
+            socket.broadcast.to(path).emit('user-connected',userId)
+            videos.set(id,userId)
+            console.log('before')
+            console.log(videos)
+            socket.on('disconnect', () => {
+                videos.delete(id);
 
-        socket.on('sending signal', payload => {
-            io.to(payload.userToSignal).emit('user joined', {
-                signal: payload.signal,
-                callerId: payload.callerId
+                socket.broadcast.to(path).emit('user-diconnected',userId)
             })
+
         })
 
-        socket.on('returning signal', payload => {
-            io.to(payload.callerId).emit('receiving returned signal', {
-                signal: payload.signal,
-                id: socket.id
-            })
+
+        socket.on('remove-video', (path) =>{
+
+            socket.join(path)
+            const data = videos.get(socket.id)
+            if(data !==undefined){
+                io.to(path).emit('refresh',socket.id)
+                socket.broadcast.to(path).emit('user-diconnected',data)
+            }
         })
+
+
 
 
     })
